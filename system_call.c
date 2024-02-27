9735268a059ff4f90745755e697a487096a94503
prateek <prateeksingh8841@gmail.com>
2024-02-26 21:28:04 -0700

    feat: sys

--- system_call.c
+++ system_call.c
@@ -2,13 +2,28 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main() {
+int main()
+{
     // using system call
+    // USER GROUP OTHER
+    // R W X
     int file;
-    write(STDERR_FILENO, "Hello World!\n", sizeof("Hello World1\n")); 
+    write(STDERR_FILENO, "Hello World!\n", sizeof("Hello World1\n"));
     file = open("hello.txt", O_RDWR | O_CREAT, 0755);
-    if(file < 0) {
+    if (file < 0)
+    {
         printf("Error\n");
     }
+
+    int x;
+    x = close(file);
+    if (x < 0)
+    {
+        printf("error");
+        return 1;
+    }
+
+    int y;
+    y = remove("hello.txt");
     return 0;
 }
