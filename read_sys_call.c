4a1ad24a3636d6a59cdbe432cb1a2ed43ed5cba1
prateek <prateeksingh8841@gmail.com>
2024-02-26 21:37:49 -0700

    feat: sys

---
+++ read_sys_call.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd;
+    char buffer[100];
+    fd = open("random_text.txt", O_RDONLY, 0777);
+    int readCount;
+    readCount = read(fd, buffer, 100);
+    if (readCount < 0)
+    {
+        return 1;
+    }
+    else
+    {
+        printf("%i\n", readCount);
+        printf("%s", buffer);
+    }
+    return 0;
+}
